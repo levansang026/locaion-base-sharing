ec11e93bb4646dfd73595687c7393d4c3b3cd5d0
feelingkilling <hoangdao9d@gmail.com>
2017-04-13 08:55:35 +0700

    small update 2

--- ios-client/location-base-sharing-sample/Pods/Target Support Files/Pods-location-base-sharing-sample/Pods-location-base-sharing-sample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_location_base_sharing_sample : NSObject
-@end
-@implementation PodsDummy_Pods_location_base_sharing_sample
-@end
--- ios-client/location-base-sharing-sample/Pods/Target Support Files/Pods-location-base-sharing-sampleUITests/Pods-location-base-sharing-sampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_location_base_sharing_sampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_location_base_sharing_sampleUITests
-@end
